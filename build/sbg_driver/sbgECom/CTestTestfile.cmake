a7cbf3dc3419b88735f7dab915607e0caa0c9deb
zf <vvspirin77@gmail.com>
2018-04-25 15:10:32 +0200

    Delete builds

--- build/sbg_driver/sbgECom/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zf/scan_viz/src/sbg_driver/sbgECom
-# Build directory: /home/zf/scan_viz/build/sbg_driver/sbgECom
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/velodyne_pointcloud/src/conversions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zf/scan_viz/src/velodyne_pointcloud/src/conversions
-# Build directory: /home/zf/scan_viz/build/velodyne_pointcloud/src/conversions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
