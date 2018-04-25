58d33c7022263578e6c17436c8c6ac6a06b9d4a1
zf <vvspirin77@gmail.com>
2018-04-25 15:08:59 +0200

    Delete builds

--- build/sbg_driver/sbgECom/CMakeFiles/sbgECom.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/zf/scan_viz/devel/lib/libsbgECom.a"
-)
--- devel/share/nmea_msgs/cmake/nmea_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(nmea_msgs_MESSAGE_FILES "/home/zf/scan_viz/src/nmea_msgs/msg/Gpgga.msg;/home/zf/scan_viz/src/nmea_msgs/msg/Gpgsa.msg;/home/zf/scan_viz/src/nmea_msgs/msg/Gpgsv.msg;/home/zf/scan_viz/src/nmea_msgs/msg/GpgsvSatellite.msg;/home/zf/scan_viz/src/nmea_msgs/msg/Gprmc.msg;/home/zf/scan_viz/src/nmea_msgs/msg/Sentence.msg")
-set(nmea_msgs_SERVICE_FILES "")
