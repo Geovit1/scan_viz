5c5a00b8332cfa4eb0d0d84fa442db8d6145e88c
zf <vvspirin77@gmail.com>
2018-04-25 15:54:55 +0200

    Delete all binaries

--- build/sbg_driver/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zf/scan_viz/src/sbg_driver
-# Build directory: /home/zf/scan_viz/build/sbg_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(sbgECom)
